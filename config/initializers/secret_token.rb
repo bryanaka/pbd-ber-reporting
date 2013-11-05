41b1b7306d86ded977ecead3907f5c0f607e4eeb
Bryan M. Robles <bryanaka0@gmail.com>
2013-11-04 17:01:56 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PbdBiz::Application.config.secret_key_base = '7478da07dae819c500479ba0fa76b88df6fd886b897bb9c3a0ed23573e71fb5de4e677d2aeaa3f5dc1d0fcf8e296cd477a32a1e1284e4fd24e3732d75eea29d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PbdBiz::Application.config.session_store :cookie_store, key: '_pbd_biz_session'
